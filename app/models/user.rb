46886ce593b42183b2fe6a940e6f1298f08bcf06
Jon Roberts <kewlguy@gmail.com>
2020-11-17 21:26:57 -0700

    added dependendant under user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@
 
 class User < ActiveRecord::Base
   extend Devise::Models
+  has_many :entries, dependent: :destroy
+  # TODO: Need to figure out if want to destroy or nullify this
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
