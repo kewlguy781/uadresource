bfd9d85b4fda938a559393ffc4266f1fd353db0a
Jon Roberts <kewlguy@gmail.com>
2020-12-02 08:09:12 -0700

    updated controller

--- app/controllers/api/catergories_controller.rb
+++ app/controllers/api/catergories_controller.rb
@@ -1,16 +1,46 @@
 class Api::CatergoriesController < ApplicationController
+before_action :set_catergory, only [:show]
+
+
   def index
+    render json: Catergory.all
   end
 
   def show
+    render json: @catergory
   end
 
   def create
+    if catergory.save
+      render json: catergory
+    else
+      render json: catergory.errors, status: 422
+    end
+
   end
 
   def update
+    if @catergory.update(catergory_params)
+      render json: @catergory
+
+    else 
+      render json: @catergory.errors, status: 422
+      return 
+    end
+
   end
 
   def destroy
+    # should we destory or change the active flag to 0?
   end
+
+  private
+  def set_catergory
+    @catergory = Category.find(params[:id])
+  end
+
+  # def catergories_params
+  #   params.permit()
+  # end
+
 end
--- app/controllers/api/entries_controller.rb
+++ app/controllers/api/entries_controller.rb
@@ -1,16 +1,45 @@
 class Api::EntriesController < ApplicationController
+# Entries, will may need to set up by user as we are controlling entry via user only
+before_action :set_entry, only [:show]
+
   def index
+    #Will need to change this as we will not display all entries, maybe by user?
+    render json: Entry.all
   end
 
   def show
+    render json: @entry
   end
 
   def create
+    if entry.save
+      render json: entry
+    else
+      render json: entry.errors, status: 422
+    end
   end
 
   def update
+    if @entry.update(entry)
+      render json: @entry
+
+    else 
+      render json: @entry.errors, status: 422
+      return 
+    end
   end
 
   def destroy
   end
+
+  private
+  def set_entry
+    @entry = Entry.find(params[:id])
+  end
+
+def entry_params
+  #TODO enter params here
+end
+
+
 end
